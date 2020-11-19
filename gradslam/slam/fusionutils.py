64279a86a7078d56701e5a699a63965ddae849c6
Soroush <soroush.saryazdi@gmail.com>
2020-11-18 20:37:05 -0500

    fix torch.unique() grad error

--- gradslam/slam/fusionutils.py
+++ gradslam/slam/fusionutils.py
@@ -519,7 +519,9 @@ def find_best_unique_correspondences(
     # used torch.unique to sort rows (rows are unique): works as if we stable sorted rows ascendingly based on every
     # column going from right to left.
     # TODO: Might be a faster way? argsort(1e10 * pc2im_bnhw[:, 0] + 1e8 * pc2im_bnhw[:, 2:] + 1e6*inv_ccounts + ...)
-    sorted_criteria = torch.unique(unique_criteria_bhwcrn, dim=0)
+    sorted_criteria = torch.unique(
+        unique_criteria_bhwcrn.detach(), dim=0
+    )  # pytorch issue #47851
     indices = sorted_criteria[:, -1].long()
 
     # find indices of the first occurrences of (sorted) duplicate B, H, W indices
